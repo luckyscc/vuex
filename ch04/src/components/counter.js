0eeee8f69ab0fb30d775ad428846254e9f07773f
songjiwei <939346832@qq.com>
2019-04-07 13:11:40 +0800

    .

--- ch04/src/components/counter.js
+++
@@ -1,9 +0,0 @@
-const Counter = {
-    template: `<div>{{ count }} {{msg}}</div>`,
-    computed: {
-        count: (state) => state.count,
-        msg: (state) => state.msg
-    }
-}
-
-export default Counter;
