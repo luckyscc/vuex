7b7bfe7c57c07e5c6897d419e0a52700f77e37c9
songjiwei <939346832@qq.com>
2019-04-08 23:52:36 +0800

    :sparkles:vuex购物车

---
+++ ch10/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import createStore from './store'
+
+Vue.use(Vuex)
+const store = createStore()
+const app = new Vue({
+    el: '#app',
+    store,
+    template: `
+        <div>
+            Hello Vue {{ $store.state.count }}
+        </div>
+    `
+})
